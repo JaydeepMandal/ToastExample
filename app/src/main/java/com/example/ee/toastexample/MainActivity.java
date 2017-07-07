3d0b91e1746c01967658b18affcb46c5410efc91
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-07-07 19:43:23 +0500

    Removed Warnings

--- app/src/main/java/com/example/ee/toastexample/MainActivity.java
+++ app/src/main/java/com/example/ee/toastexample/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.ee.toastexample;
 
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
