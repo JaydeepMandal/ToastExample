81c5a2392124f601a4857abd98a801aae159b1ba
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-07-07 13:31:46 +0500

    changed the location of toast

--- app/src/main/java/com/example/ee/toastexample/MainActivity.java
+++ app/src/main/java/com/example/ee/toastexample/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.ee.toastexample;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
     }
     public void clickable(View v){
         Toast tost = Toast.makeText(getApplicationContext(),R.string.toast,Toast.LENGTH_LONG);
+        tost.setGravity(Gravity.CLIP_HORIZONTAL,0,0);
         tost.show();
     }
 }
