d469174be6e725a32e399e29a24d4620cfa65331
Jaydeep Mandal <jaydeepmandal1997@gmail.com>
2017-07-07 20:58:31 +0500

    custom toast

--- app/src/main/java/com/example/ee/toastexample/MainActivity.java
+++ app/src/main/java/com/example/ee/toastexample/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.ee.toastexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
@@ -16,8 +19,13 @@ public class MainActivity extends AppCompatActivity {
         tv = (TextView) findViewById(R.id.textView);
     }
     public void clickable(View v){
-        Toast tost = Toast.makeText(getApplicationContext(),R.string.toast,Toast.LENGTH_LONG);
-        tost.setGravity(Gravity.CLIP_HORIZONTAL,0,0);
+        LayoutInflater inflate = getLayoutInflater();
+        View layout = inflate.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.custom_toast_container));
+
+        Toast tost = new Toast(getApplicationContext());
+        tost.setGravity(Gravity.CLIP_HORIZONTAL,0,-50);
+        tost.setDuration(Toast.LENGTH_LONG);
+        tost.setView(layout);
         tost.show();
     }
 }
